066c9774080582696fe5a45e5cbc69eba88d2145
Jordan Young <jordanyoung@Jordans-MacBook-Pro.local>
2025-01-26 13:04:06 -0600

    formatting

--- app/vgg.py
+++ app/vgg.py
@@ -10,15 +10,14 @@ class VGGA(torch.nn.Module):
         """Initialize VGG A Model."""
         super(VGGA, self).__init__()
 
-        self.a_1 = torch.nn.Conv2d(3, 64, (3,3), stride=1, padding=1)
-        self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2,2), stride=2)
-        self.a_2 = torch.nn.Conv2d(64, 128, (3,3), strid=1, padding=1)
+        self.a_1 = torch.nn.Conv2d(3, 64, (3, 3), stride=1, padding=1)
+        self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.a_2 = torch.nn.Conv2d(64, 128, (3, 3), strid=1, padding=1)
 
     def forward(self, x: torch.Tensor):
         """Model Forward Pass."""
-
-
         pass
+
 
 class VGGALrn(torch.nn.Module):
     """VGG Implementation Class."""
@@ -27,15 +26,14 @@ class VGGALrn(torch.nn.Module):
         """Initialize VGG A-lrn Model."""
         super(VGGALrn, self).__init__()
 
-        self.a_1 = torch.nn.Conv2d(3, 64, (3,3), stride=1, padding=1)
-        self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2,2), stride=2)
-        self.a_2 = torch.nn.Conv2d(64, 128, (3,3), strid=1, padding=1)
+        self.a_1 = torch.nn.Conv2d(3, 64, (3, 3), stride=1, padding=1)
+        self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.a_2 = torch.nn.Conv2d(64, 128, (3, 3), strid=1, padding=1)
 
     def forward(self, x: torch.Tensor):
         """Model Forward Pass."""
-
-
         pass
+
 
 class VGGB(torch.nn.Module):
     """VGG Implementation Class."""
@@ -44,65 +42,58 @@ class VGGB(torch.nn.Module):
         """Initialize VGG B Model."""
         super(VGGB, self).__init__()
 
-        self.a_1 = torch.nn.Conv2d(3, 64, (3,3), stride=1, padding=1)
-        self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2,2), stride=2)
-        self.a_2 = torch.nn.Conv2d(64, 128, (3,3), strid=1, padding=1)
+        self.a_1 = torch.nn.Conv2d(3, 64, (3, 3), stride=1, padding=1)
+        self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.a_2 = torch.nn.Conv2d(64, 128, (3, 3), strid=1, padding=1)
 
     def forward(self, x: torch.Tensor):
         """Model Forward Pass."""
-
-
         pass
 
 
 class VGGC(torch.nn.Module):
     """VGG Implementation Class."""
 
-def __init__(self):
-    """Initialize VGG C Model."""
-    super(VGGC, self).__init__()
+    def __init__(self):
+        """Initialize VGG C Model."""
+        super(VGGC, self).__init__()
 
-    self.a_1 = torch.nn.Conv2d(3, 64, (3,3), stride=1, padding=1)
-    self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2,2), stride=2)
-    self.a_2 = torch.nn.Conv2d(64, 128, (3,3), strid=1, padding=1)
+        self.a_1 = torch.nn.Conv2d(3, 64, (3, 3), stride=1, padding=1)
+        self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.a_2 = torch.nn.Conv2d(64, 128, (3, 3), strid=1, padding=1)
 
-def forward(self, x: torch.Tensor):
-    """Model Forward Pass."""
+    def forward(self, x: torch.Tensor):
+        """Model Forward Pass."""
+        pass
 
-
-    pass
 
 class VGGD(torch.nn.Module):
     """VGG Implementation Class."""
 
-def __init__(self):
-    """Initialize VGG D Model."""
-    super(VGGD, self).__init__()
+    def __init__(self):
+        """Initialize VGG D Model."""
+        super(VGGD, self).__init__()
 
-    self.a_1 = torch.nn.Conv2d(3, 64, (3,3), stride=1, padding=1)
-    self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2,2), stride=2)
-    self.a_2 = torch.nn.Conv2d(64, 128, (3,3), strid=1, padding=1)
+        self.a_1 = torch.nn.Conv2d(3, 64, (3, 3), stride=1, padding=1)
+        self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.a_2 = torch.nn.Conv2d(64, 128, (3, 3), strid=1, padding=1)
 
-def forward(self, x: torch.Tensor):
-    """Model Forward Pass."""
-
-
-    pass
+    def forward(self, x: torch.Tensor):
+        """Model Forward Pass."""
+        pass
 
 
 class VGGE(torch.nn.Module):
     """VGG Implementation E Class."""
 
-def __init__(self):
-    """Initialize VGG Model."""
-    super(VGGE, self).__init__()
+    def __init__(self):
+        """Initialize VGG Model."""
+        super(VGGE, self).__init__()
 
-    self.a_1 = torch.nn.Conv2d(3, 64, (3,3), stride=1, padding=1)
-    self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2,2), stride=2)
-    self.a_2 = torch.nn.Conv2d(64, 128, (3,3), strid=1, padding=1)
+        self.a_1 = torch.nn.Conv2d(3, 64, (3, 3), stride=1, padding=1)
+        self.a_mx_1 = torch.nn.MaxPool2d(kernel_size=(2, 2), stride=2)
+        self.a_2 = torch.nn.Conv2d(64, 128, (3, 3), strid=1, padding=1)
 
-def forward(self, x: torch.Tensor):
-    """Model Forward Pass."""
-
-
-    pass
+    def forward(self, x: torch.Tensor):
+        """Model Forward Pass."""
+        pass
